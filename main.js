77a1a7082ba6f6782060e87e0967cde13e8c2768
sourajeetmohanty <m762myfavgun@gmail.com>
2020-09-13 20:01:13 +0530

    add node server using express

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path');
+
+const server = express()
+const port = 3000
+
+server.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+server.use(express.static(path.join(__dirname, 'public')));
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
